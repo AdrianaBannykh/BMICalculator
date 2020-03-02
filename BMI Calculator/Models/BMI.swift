d2ee70934d9376530a2fb5b1a830d6a930bbd3a3
Adriana Bannykh <adrianabannyh@mail.ru>
2020-03-02 16:56:59 +0500

    initial commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,21 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Adriana Bannykh on 01.03.2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+    
+    /*init(value: Float, advice: String, color: UIColor) {
+        self.value = value
+        self.advice = advice
+        self.UIColor = color
+    }*/
+}
---
+++ ResultViewController.swift
@@ -0,0 +1,45 @@
+//
+//  ResultViewController.swift
+//  BMI Calculator
+//
+//  Created by Adriana Bannykh on 27.02.2020.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class ResultViewController: UIViewController {
+
+    var bmiValue: String?
+    var advice: String?
+    var color: UIColor?
+    @IBOutlet weak var bmiLabel: UILabel!
+    
+    @IBOutlet weak var adviceLabel: UILabel!
+    
+    @IBOutlet weak var colorBackground: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        bmiLabel.text = bmiValue
+        adviceLabel.text = advice
+        //colorBackground.backgroundColor = color
+        view.backgroundColor = color
+    }
+    
+    @IBAction func recaculatePressed(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
